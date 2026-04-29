b8786d0913d9a70c16966c6d46da900d899b67e3
WrsRosanne <149767572+WrsRosanne@users.noreply.github.com>
2026-04-29 15:41:46 -0400

    about the project page

---
+++ streamlit_pages/project.py
@@ -0,0 +1,32 @@
+import streamlit as st
+def show():
+    st.markdown("# Project Motivation")
+    st.markdown("Enthusiastic New Yorkers embrace what is colloquially referred to as the \"concgrete\" jungle. Constantly facing buildings, street noise, and apartment buildings, their natural or humanistic life is often sacrificed at the expense of a vibrant and cultural city. 
+                "\n\n"
+                "In this analysis, we set out to try and tell a data-driven story about whether New Yorkers are truly inhabitants of a \"concrete jungle\" or if there are ample opportunities to still engage with wildlife. In our consideration of parks, we try to understand (from a mapping perspective) the prevalence of parks in New York. In our consideration of rodents and squirrels, we try to understand how much these city environments are corrupted by rodents and less pleasurable living acquaintances. TO BE UPDATED AFTER APRIL 28 SUBMISSION"
+                "\n\n")
+    st.markdown("# Methodology")
+    st.markdowne("We utilized geospatial and text analysis to answer these questions: \n"  
+                "1) the location and establishment of parks, greenspaces, and trees around the city, and their accessibility for residents;  \n"  
+                "2) rodent complaints by borough, zip code, and transportation lines and their relationship to demographic factors; \n"  
+                "3) Central Park squirrel sightings, including text analysis of the Squirrel Census.\n" 
+                "\n\n")
+     st.markdown("# Data Sources")
+     st.markdown("-Department of Parks and Recreation (DPR) Parks Properties (NYC OpenData)\n"
+                "-Department of Housing Preservation and Development (HPD) Affordable Housing Production by Building (NYC OpenData)\n"
+                "-Department of Parks and Recreation (DPR) 2015 Street Tree Census - Tree Data (NYC OpenData)\n"
+                "-New York City Housing Authority (NYCHA) NYCHA Public Housing Developments (NYC OpenData)\n"
+                "-Department of Parks and Recreation (DPR) Parks Zones (NYC OpenData)\n"
+                "-Department of City Planning (DCP) Borough Boundaries Dataset (NYC OpenData)\n"
+                "-United States Census Bureau 2025 TIGER/Line® Shapefiles: ZIP Code Tabulation Areas\n"
+                "-United States Census Bureau American Community Survey 5-Year Data (2009-2024)\n"
+                "-NYU Spatial Data Repository 2016 (May) New York City Subway Routes  \n"
+                "-Department of City Planning (DCP) 2020 Neighborhood Tabulation Areas (NTAs)\n"
+                "-Metropolitan Transportation Authority MTA Subway Stations\n"
+                "-The Squirrel Census 2018 Central Park Squirrel Census - Squirrel Data (NYC OpenData)\n"
+                "-The Squirrel Census 2018 Central Park Squirrel Census - Stories (NYC OpenData)\n"
+                "-311 Service Requests from 2020 to Present (NYC OpenData)\n"
+                "-Department of Buildings (DOB) DOB NOW: Build – Approved Permits (NYC OpenData)\n"
+                "-Department of Health and Mental Hygiene (DOHMH) DOHMH New York City Restaurant Inspection Results (NYC OpenData)\n"
+                "-United States Census Bureau \n"
+                )
